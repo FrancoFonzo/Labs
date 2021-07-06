c14ad4f6e32c449f2cf3f7de4434d7cc79ccdc8c
Franco Fonzo <francofonzo1@gmail.com>
2021-07-06 11:57:54 -0300

    Carga Lab4.3 + Carga de Laboratorio 4.3 incompelto. + Ejercicio1 (DataTables), Ejercicio2 (DataSet no tipado).

---
+++ Unidad.4.Lab.3.ADO.NetSQLyDataBinding/Lab4.3/Ejercicio1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dtAlumnos = new DataTable();
+            DataRow rwAlumno = dtAlumnos.NewRow();
+
+            DataColumn dtColNombre = new DataColumn("Nombre");
+            DataColumn dtColApellido = new DataColumn("Apellido");
+
+            dtAlumnos.Columns.Add(dtColNombre);
+            dtAlumnos.Columns.Add(dtColApellido);
+
+            rwAlumno[dtColNombre] = "Franco";
+            rwAlumno[dtColApellido] = "Fonzo";
+
+            dtAlumnos.Rows.Add(rwAlumno);
+
+            DataRow rwAlumno2 = dtAlumnos.NewRow();
+            rwAlumno2[dtColNombre] = "Danny";
+            rwAlumno2[dtColApellido] = "Carey";
+            dtAlumnos.Rows.Add(rwAlumno2);
+
+            DataRow rwAlumno3 = dtAlumnos.NewRow();
+            rwAlumno3[dtColNombre] = "Mario";
+            rwAlumno3[dtColApellido] = "Duplantier";
+            dtAlumnos.Rows.Add(rwAlumno3);
+
+            Console.WriteLine("Listado de Alumnos\n");
+
+            foreach (DataRow rw in dtAlumnos.Rows)
+            {
+                Console.WriteLine($"{rw.Field<String>(dtColApellido)}, " +
+                    $"{rw.Field<String>(dtColNombre)}");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Unidad.4.Lab.3.ADO.NetSQLyDataBinding/Lab4.3/Ejercicio2/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dtAlumnos = new DataTable("Alumnos");
+
+            DataColumn colIdAlumno = new DataColumn("ID", typeof(int))
+            {
+                ReadOnly = true,        
+                AutoIncrement = true,
+                AutoIncrementSeed = 0,
+                AutoIncrementStep = 1
+            };
+            DataColumn colNombreAlumno = new DataColumn("Nombre", typeof(string));
+            DataColumn colApellidoAlumno = new DataColumn("Apellido", typeof(string));
+
+            dtAlumnos.Columns.Add(colIdAlumno);
+            dtAlumnos.Columns.Add(colNombreAlumno);
+            dtAlumnos.Columns.Add(colApellidoAlumno);
+
+            // La PK puede estar formada por varios atributos,
+            // por lo que pide un arreglo de columnas
+            dtAlumnos.PrimaryKey = new DataColumn[] { colIdAlumno };
+
+            DataRow rwAlumno = dtAlumnos.NewRow();
+            rwAlumno[colNombreAlumno] = "Franco";
+            rwAlumno[colApellidoAlumno] = "Fonzo";
+
+            dtAlumnos.Rows.Add(rwAlumno);
+
+            DataRow rwAlumno2 = dtAlumnos.NewRow();
+            rwAlumno2[colNombreAlumno] = "Danny";
+            rwAlumno2[colApellidoAlumno] = "Carey";
+            dtAlumnos.Rows.Add(rwAlumno2);
+
+            DataRow rwAlumno3 = dtAlumnos.NewRow();
+            rwAlumno3[colNombreAlumno] = "Mario";
+            rwAlumno3[colApellidoAlumno] = "Duplantier";
+            dtAlumnos.Rows.Add(rwAlumno3);
+
+            DataTable dtCursos = new DataTable("Cursos");
+
+            DataColumn colIdCurso = new DataColumn("ID", typeof(int)) 
+            {
+                ReadOnly = true,
+                AutoIncrement = true,
+                AutoIncrementSeed = 0,
+                AutoIncrementStep = 1
+            };
+            DataColumn colCurso = new DataColumn("Curso", typeof(string));
+
+            dtCursos.Columns.Add(colIdCurso);
+            dtCursos.Columns.Add(colCurso);
+
+            dtCursos.PrimaryKey = new DataColumn[] { colIdCurso };
+
+            DataRow rwCurso = dtCursos.NewRow();
+            rwCurso[colCurso] = ".NET";
+            dtCursos.Rows.Add(rwCurso);
+
+            DataSet dsUniversidad = new DataSet("Universidad");
+            dsUniversidad.Tables.Add(dtCursos);
+            dsUniversidad.Tables.Add(dtAlumnos);
+
+            // Realcion: dtCursos  1:N  ------  0:M  dtAlumnos
+            DataTable dtCursosAlumnos = new DataTable("CursosAlumno");
+
+            DataColumn colCAIdCurso = new DataColumn("IDCurso", typeof(int));
+            DataColumn colCAIdAlumno = new DataColumn("IDAlumno", typeof(int));
+            dtCursosAlumnos.Columns.Add(colCAIdAlumno);
+            dtCursosAlumnos.Columns.Add(colCAIdCurso);
+
+            dsUniversidad.Tables.Add(dtCursosAlumnos);
+
+            DataRelation relAlumnoAC = 
+                new DataRelation("AlumnoCursos", colIdAlumno, colCAIdAlumno);
+            DataRelation relCursoAC =
+                new DataRelation("CursoAlumnos", colIdCurso, colCAIdCurso);
+            dsUniversidad.Relations.Add(relAlumnoAC);
+            dsUniversidad.Relations.Add(relCursoAC);
+
+            DataRow rwCursosAlumnos = dtCursosAlumnos.NewRow();
+            rwCursosAlumnos[colCAIdAlumno] = 0; //Alumno: Franco Fonzo
+            rwCursosAlumnos[colCAIdCurso] = 0;  //Curso: .NET
+            dtCursosAlumnos.Rows.Add(rwCursosAlumnos);
+
+            rwCursosAlumnos = dtCursosAlumnos.NewRow();
+            rwCursosAlumnos[colCAIdAlumno] = 1; //Alumno: Danny Carey
+            rwCursosAlumnos[colCAIdCurso] = 0;  //Curso: .NET
+            dtCursosAlumnos.Rows.Add(rwCursosAlumnos);
+
+            rwCursosAlumnos = dtCursosAlumnos.NewRow();
+            rwCursosAlumnos[colCAIdAlumno] = 2; //Alumno: Mario Duplantier
+            rwCursosAlumnos[colCAIdCurso] = 0;  //Curso: .NET
+            dtCursosAlumnos.Rows.Add(rwCursosAlumnos);
+
+            Console.Write("Ingrese nombre del curso: ");
+            string curso = Console.ReadLine();
+            Console.WriteLine($"\nListado de alumnos del curso {curso}\n");
+            DataRow[] rowCursos = dtCursos.Select($"Curso = '{curso}'");
+
+            foreach (DataRow rowCurso in rowCursos)
+            {
+                DataRow[] rowAlumnos = rowCurso.GetChildRows(relCursoAC);
+                foreach (DataRow rowAlumno in rowAlumnos)
+                {
+                    Console.WriteLine(
+                        $"{rowAlumno.GetParentRow(relAlumnoAC)[colApellidoAlumno].ToString()}" +
+                        $", " +
+                        $"{rowAlumno.GetParentRow(relAlumnoAC)[colNombreAlumno].ToString()}"
+                        );
+                }
+            }
+        }
+    }
+}
